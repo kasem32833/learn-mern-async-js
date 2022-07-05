245c77ab68512bf4c8824fe711529ffe1b1954f6
kasem32833 <abulkasem32833@gmail.com>
2022-07-05 10:48:19 +0300

    1 file modified

--- index.js
+++ index.js
@@ -2,10 +2,40 @@
 import './style.css';
 
 // Write Javascript code!
-console.log('Hello gyes.');
+console.log('Hello friends.');
 
 
+// sync Callback Function
+const syncCallbackFunction = (index, start) => {
+  const numIsPrime = isPrime(index);
+  console.log('Prime: ', index, numIsPrime, start, new Date().getSeconds());
+};
 
+
+const sleepWithCallback = (num, callback) =>{
+  const start = Date.now();
+  const sleep = num * 1000
+  while (true){
+    if (Date.now() - start > num) break;
+  }
+  callback(num, new Date(start));
+}
+
+
+//sync with callback Function
+
+const syncWithCallback = () => {
+  for (let index = 0; index < 10; index++){
+    console.log('sleeping: ', index , new Date());
+    sleepWithCallback(index , syncCallbackFunction);
+
+  }
+}
+
+
+syncWithCallback();
+
+console.log('all done')
 
 
 
