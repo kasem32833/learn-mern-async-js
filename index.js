4420e5d80ebbfe184370553dd338d9300f873101
kasem32833 <abulkasem32833@gmail.com>
2022-07-05 08:55:30 +0300

    1 file modified

--- index.js
+++ index.js
@@ -2,5 +2,14 @@
 import './style.css';
 
 // Write Javascript code!
+console.log('Hello gyes.');
+
+
+
+
+
+
+
+
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Learn Mern Like a pro</h1>`;
